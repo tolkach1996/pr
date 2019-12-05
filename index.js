de911011f64f298f33c9a1c25385d647b644a463
tolkach96 <46722942+tolkach96@users.noreply.github.com>
2019-12-06 00:24:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+const database = require('./database');
+const config = require('./config');
+
+database()
+  .then(info => {
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    app.listen(config.PORT, () =>
+      console.log(`Example app listening on port ${config.PORT}!`)
+    );
+  })
+  .catch((e) => {
+    console.error(e)
+    console.error('Unable to connect to database');
+    process.exit(1);
+  });
